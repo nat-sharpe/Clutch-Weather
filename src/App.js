c4d86684826d1cc2c51affa5cf5a47b23034e46d
Nat Sharpe <sharpe1890@gmail.com>
2018-10-27 17:44:03 -0400

    Stores condition and forecast data in app.js state

--- src/App.js
+++ src/App.js
@@ -6,44 +6,49 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      locations: ['Atlanta', 'San Francisco'],
-      currentLocation: {
-        city: 'Atlanta',
-        state: 'GA'
-      },
-      forecast: [
+      savedLocations: [
         {
-          day: 'Sat',
-          high: '32',
-          low: '28',
-          text: 'Sunny'     
-        },
-        {
-          day: 'Sun',
-          high: '31',
-          low: '26',
-          text: 'Mostly Sunny'     
+          city: 'Atlanta',
+          state: 'GA'
         }
-      ]
+      ],
+      currentLocation: 'Atlanta, GA',
+      currentCondition: {},
+      currentForecast: []
     };
   };
 
   componentDidMount() {
     const getWeather = () => {
-      fetch(`https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${this.state.currentLocation.city}, ${this.state.currentLocation.state}")&format=json`)
+      fetch(`https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${this.state.currentLocation}")&format=json`)
         .then(data => {
           const jsonResult = data.json();
           jsonResult.then(parsedData => {
-            console.log(parsedData.query.results.channel.location.city);
-            console.log(parsedData.query.results.channel.item.condition.text);
-            console.log(parsedData.query.results.channel.item.condition.temp);
+            const conditionData = {
+              city: parsedData.query.results.channel.location.city,
+              text: parsedData.query.results.channel.item.condition.text,
+              temp: parsedData.query.results.channel.item.condition.temp
+            };
+            this.setState({
+              currentCondition: conditionData
+            })
+            console.log(this.state.currentCondition)
             const nextFourDays = parsedData.query.results.channel.item.forecast;
             for (let ii = 0; ii < 4; ii++) {
-              console.log(nextFourDays[ii].day);
-              console.log(nextFourDays[ii].text);
-              console.log(nextFourDays[ii].high);
-              console.log(nextFourDays[ii].low);
+              let dayData = {
+                day: nextFourDays[ii].day,
+                text: nextFourDays[ii].text,     
+                high: nextFourDays[ii].high,
+                low: nextFourDays[ii].low
+              };
+              this.setState({ 
+                currentForecast: [
+                  ...this.state.currentForecast, 
+                  dayData
+                ]
+              }); 
             };
+            console.log(this.state.currentForecast)
           });
         }) 
     };
