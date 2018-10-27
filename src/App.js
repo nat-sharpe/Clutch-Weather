5e3bb55601ce9604d778ef7f1d111f93b93cf8e3
Nat Sharpe <sharpe1890@gmail.com>
2018-10-27 16:59:46 -0400

    Inits state in app.js and fetches Yahoo API data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React, {Component} from 'react';
 import Main from './components/main';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: ['Atlanta', 'San Francisco'],
+      currentLocation: 'Atlanta'
+    };
+  };
+
+  componentDidMount() {
+    const getWeather = () => {
+      fetch(`https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${this.state.currentLocation}")&format=json`)
+        .then(data => {
+          console.log(data);
+          const jsonResult = data.json();
+          jsonResult.then(parsedData => {
+            console.log(parsedData.query.results.channel.item.condition.temp);
+          });
+        }) 
+    };
+    getWeather();
+  };
+
   render() {
     return (
       <Main />
