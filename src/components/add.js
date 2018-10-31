949b430755a5dfd148d86755c2ef3facd7ac4b0e
Nat Sharpe <sharpe1890@gmail.com>
2018-10-30 23:53:52 -0400

    Creates Add component

---
+++ src/components/add.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Add extends Component {
+  render() {
+    return (
+      <div className="add">
+        <p>Add input fields here</p>
+      </div>
+    );
+  };
+};
+
+export default Add;
