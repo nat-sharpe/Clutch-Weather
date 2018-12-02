88650a3232b9645c00d9a260d45095ef89210dab
Nat Sharpe <sharpe1890@gmail.com>
2018-12-02 15:34:53 -0500

    User can now toggle left as well as right'

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ class App extends Component {
   };
 
   getWeather = () => {
-    console.log('city' + this.state.currentLocation.city)
     fetch(`https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${this.state.currentLocation.city}, ${this.state.currentLocation.region}")&format=json`)
     .then(data => {
       const jsonResult = data.json();
@@ -47,7 +46,6 @@ class App extends Component {
         this.setState({
           currentCondition: conditionData
         });
-        console.log(this.state.currentCondition)
         const nextFourDays = parsedData.query.results.channel.item.forecast;
         let dayArray = [];
         for (let ii = 0; ii < 4; ii++) {
@@ -70,26 +68,37 @@ class App extends Component {
     this.getWeather();
   };
 
-  toggleLocation = () => {
+  toggleLocation = (direction) => {
+  
     let nextIndex; 
-    if (this.state.currentLocation.index === (this.state.savedLocations.length - 1)) {
-      nextIndex = 0; 
-    } else {
-      nextIndex = (this.state.currentLocation.index + 1); 
-    };
+    let currentIndex = this.state.currentLocation.index;
+
+    if (direction === 'left') {
+      nextIndex = (currentIndex === 0) ? this.state.savedLocations.length - 1 : currentIndex -1; 
+    } else if (direction === 'right') {
+      nextIndex = (currentIndex === this.state.savedLocations.length - 1) ? 0 : currentIndex + 1; 
+    }
+
     const newLocation = {
       city: this.state.savedLocations[nextIndex].city,
       region: this.state.savedLocations[nextIndex].region,
       index: nextIndex
     };
-    console.log(newLocation);
     this.setState({ 
       currentLocation: newLocation
     }, 
       this.getWeather);
   };
 
-  everyLocation = () => {
+  toggleLeft = () => {
+    this.toggleLocation('left');
+  };
+
+  toggleRight = () => {
+    this.toggleLocation('right');
+  };
+
+  viewLocations = () => {
     this.setState({ 
       visiblePage: 'every',
     })
@@ -97,7 +106,12 @@ class App extends Component {
 
   render() {
     if (this.state.visiblePage === 'single') {
-      return <SingleLocation state={this.state} toggleLocation={this.toggleLocation} everyLocation={this.everyLocation} />
+      return <SingleLocation 
+        state={this.state} 
+        toggleLeft={this.toggleLeft}
+        toggleRight={this.toggleRight}  
+        viewLocations={this.viewLocations} 
+      />
     } else if (this.state.visiblePage === 'every') {
       return <EveryLocation state={this.state} />
     } else if (this.state.visiblePage === 'add') {
--- src/containers/SingleLocation.js
+++ src/containers/SingleLocation.js
@@ -4,37 +4,39 @@ class SingleLocation extends Component {
   render() {
     const forecastRows = [];
     const forecastData = this.props.state.currentForecast;
+
     forecastData.forEach((dayData, index) => {
       let day = (index === 0) ? 'Today' : dayData.day;
       forecastRows.push(
         <li className="forecast-row" key={index}>
-          <p>{day}</p>
-          <p>{dayData.text}</p>
-          <p>{dayData.high}°</p>
-          <p>{dayData.low}°</p>
+          <p className="row-day">{day}</p>
+          <p className="row-weather">{dayData.text}</p>
+          <p className="row-high">{dayData.high}°</p>
+          <p className="row-low">{dayData.low}°</p>
         </li>
       )
     });
+
     return (
-      <div className="main">
-        <div className="header">
-          <h1>Clutch Weather</h1>
-        </div>
+      <div className="single-main">
         <div className="current">
-          <h2>{this.props.state.currentCondition.city}</h2>
-          <h3>{this.props.state.currentCondition.text}</h3>
-          <h1>{this.props.state.currentCondition.temp}°</h1>
+          <h2 className="current-city">{this.props.state.currentCondition.city}</h2>
+          <p className="current-weather" >{this.props.state.currentCondition.text}</p>
+          <h1 className="current-temp">{this.props.state.currentCondition.temp}°</h1>
         </div>
         <ul className="forecast">
           {forecastRows}
         </ul>
-        <div className="nav">
-          <div className="location-toggle">
-            <button onClick={this.props.toggleLocation} type="button">Toggle Locations</button>
+        <div className="footer">
+          <div className="toggle-left">
+            <button onClick={this.props.toggleLeft} type="button">{'<'}</button>
           </div>  
           <div className="location-all">
-            <button onClick={this.props.chooseLocation} type="button">All Locations</button>
+            <button onClick={this.props.viewLocations} type="button">Your Locations</button>
           </div> 
+          <div className="toggle-left">
+            <button onClick={this.props.toggleRight} type="button">{'>'}</button>
+          </div>  
         </div>
       </div>
     );
