e2b49b6ada1c4c7a149e00ff01b3415b2a062196
Nat Sharpe <sharpe1890@gmail.com>
2018-12-03 03:23:54 -0500

    Updates

---
+++ precache-manifest.a7a09be7ca6419cf038b833c708c21e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d21706f0006b459e7f20",
+    "url": "/Clutch-Weather/static/js/runtime~main.d21706f0.js"
+  },
+  {
+    "revision": "bd7f200cf2a56775da68",
+    "url": "/Clutch-Weather/static/js/main.bd7f200c.chunk.js"
+  },
+  {
+    "revision": "c0192f5fb0f66adb0721",
+    "url": "/Clutch-Weather/static/js/1.c0192f5f.chunk.js"
+  },
+  {
+    "revision": "bd7f200cf2a56775da68",
+    "url": "/Clutch-Weather/static/css/main.fd1105fa.chunk.css"
+  },
+  {
+    "revision": "6efa6ea2729e12db043c05bd06dbcb2d",
+    "url": "/Clutch-Weather/index.html"
+  }
+];
